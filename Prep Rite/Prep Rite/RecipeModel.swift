26b6ff8a9a878ef3d135b986295a1adedef1c4dc
anya-westby <79611799+anya-westby@users.noreply.github.com>
2022-02-23 17:10:41 -0700

    Trial 1

---
+++ Prep Rite/Prep Rite/RecipeModel.swift
@@ -0,0 +1,14 @@
+//
+//  RecipeModel.swift
+//  Prep Rite
+//
+//  Created by Anya Westby on 2/19/22.
+//
+
+import UIKit
+
+class RecipeModel: NSObject {
+    var meal: String = ""
+    var pro: String = ""
+    var veg: String = ""
+}
